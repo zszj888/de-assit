1d36163939a9d24982cebd0a8b4fdecbbe35ca5d
ZHANJ596 <sam.x.zhang.-nd@disney.com>
2021-01-08 09:17:31 +0800

    first commit

---
+++ src/main/java/com/somecom/deassistant/DeAssistApplication.java
@@ -0,0 +1,15 @@
+package com.somecom.deassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeAssistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeAssistApplication.class, args);
+    }
+
+
+
+}
